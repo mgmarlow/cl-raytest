ac75a07bc3f2382b88f3b3ca9a1523543032fc2b
Graham Marlow <graham@mgmarlow.com>
2024-01-04 15:21:44 -0800

    Don't write to *global-registry*
    Instead, use source-registry.conf.d.

--- dev.lisp
+++ dev.lisp
@@ -1,5 +1,2 @@
-(push "~/projects/raytest/raylib/" asdf:*central-registry*)
-(push "~/projects/raytest/" asdf:*central-registry*)
 (asdf:load-system "game")
-
 (game::main)
--- release.lisp
+++ release.lisp
@@ -1,3 +1,1 @@
-(push "~/projects/raytest/raylib/" asdf:*central-registry*)
-(push "~/projects/raytest/" asdf:*central-registry*)
 (asdf:make "game/executable")
